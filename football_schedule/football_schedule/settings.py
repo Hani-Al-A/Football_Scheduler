92787aa81bd89e1a34b192c8b6abadd75476342e
Hani-Al-A <hani.al.a26@gmail.com>
2025-07-09 18:05:04 +0400

    Update settings.py

--- football_schedule/football_schedule/settings.py
+++ football_schedule/football_schedule/settings.py
@@ -35,8 +35,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'totslewanah@gmail.com'
-EMAIL_HOST_PASSWORD = 'tesz ugom uaaa qaod'
+EMAIL_HOST_USER = #
+EMAIL_HOST_PASSWORD = #
 
 
 
