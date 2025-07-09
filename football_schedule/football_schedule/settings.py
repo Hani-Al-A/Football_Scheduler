a607e936901404d34e03e052d86f28ded0c85571
Hani Anani <hanoo@Mac-zift.local>
2025-07-09 16:20:21 +0400

    more mobile-responsive

--- football_schedule/football_schedule/settings.py
+++ football_schedule/football_schedule/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = #key
+SECRET_KEY = 'django-insecure-syn_crby@ck*n(-*p#^pq2skquj9#gg^520=gyk)*k5(mbf%xq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -35,8 +35,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = #user
-EMAIL_HOST_PASSWORD = #password
+EMAIL_HOST_USER = 'totslewanah@gmail.com'
+EMAIL_HOST_PASSWORD = 'tesz ugom uaaa qaod'
+
 
 
 # Application definition
